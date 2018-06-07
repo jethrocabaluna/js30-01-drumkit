c809cf5f47ef64eb590626ceaf7ebb173c3ded61
Jethro Cabaluna <jethrocabaluna310@gmail.com>
2018-06-07 13:30:39 +0800

    modify script

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+}
+
 function removeTransition(e) {
     if (e.propertyName !== "transform") return;
     this.classList.remove("playing");
 }
 
 document.addEventListener("DOMContentLoaded", e => {
-    document.addEventListener("keypress", e => {
-
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
-        if (!audio) return;
-        key.classList.add("playing");
-        audio.currentTime = 0;
-        audio.play();
-    });
+    document.addEventListener("keypress", playSound);
 
     const keys = document.querySelectorAll(".key");
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
